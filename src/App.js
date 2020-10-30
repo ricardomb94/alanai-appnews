bb6eec89fca541606eca6a221843434a776e712e
ricardomb94 <aymardmb@gmail.com>
2020-10-30 09:40:48 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
+//we declare the alan ai key as a variable
+const alankey =
+  'fb41afe211119f525dad6c4faef5e89d2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+  useEffect(() => {
+    alanBtn({
+      //let set alan ai's API key
+      key: alankey,
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Alan AI News Application</h1>
     </div>
   );
-}
-
+};
 export default App;
