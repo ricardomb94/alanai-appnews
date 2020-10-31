566d540d5e53dcc8523465e7458cda357b79e250
ricardomb94 <aymardmb@gmail.com>
2020-10-31 03:25:30 +0100

    Display news after play command on Alan Ai studio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-//we declare the alan ai key as a variable
+import NewsCards from './components/NewsCards/NewsCards';
+
+//we declare the alan API key as a variable
 const alankey =
   'fb41afe211119f525dad6c4faef5e89d2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
   useEffect(() => {
     alanBtn({
       //let set alan ai's API key
       key: alankey,
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+        }
+      },
     });
   }, []);
   return (
     <div>
       <h1>Alan AI News Application</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 };
 export default App;
+
+// Newsapi:42f13a6e6b5f4625a57e9b31c187f020
